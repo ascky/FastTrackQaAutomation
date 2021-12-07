080f85ec96c6684d2952b717e4fe9d9e0549b3a3
Cristian Astilean <cristian.astilean@itiviti.com>
2021-12-07 19:36:30 +0200

    Add methods homework solution

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,3 +1,29 @@
 public class Calculator {
 
+    public double averageThreeNumbers(int nrOne, int nrTwo, int nrThree){
+        return (nrOne + nrTwo + nrThree) / 3d;
+    }
+
+    public int divisionModuloDisplay(int nrOne,int nrTwo ){
+        return nrOne % nrTwo;
+    }
+
+    public int fahrenheitToCelsius(int fahrenheit){
+         float celsius = 5/9f * (fahrenheit -32);
+         return (int) celsius;
+    }
+
+    public double inchToMeter(double inch){
+        return 0.0254 * inch;
+    }
+
+    public void speedCalculator (double distance, double hours, double minutes, double seconds){
+        double speedMetersPerSecond = distance / (hours * 60 * 60 + minutes * 60 + seconds);
+        System.out.println("Speed in Meters per second is " + speedMetersPerSecond);
+        double speedKilometersPerHour = (speedMetersPerSecond * 3600) / 1000;
+        System.out.println("Speed in KM per Hour is "+ speedKilometersPerHour);
+        double speedMilesPerHour = speedKilometersPerHour / 1.609;
+        System.out.println("Speed in MILES per Hour is "+ speedMilesPerHour);
+        //return speedMetersPerSecond;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,31 +3,66 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
-//        conversieFahrenheitToCelsius(44);
-//        conversieFahrenheitToCelsius(77);
-//        conversieFahrenheitToCelsius(12);
-//        conversieFahrenheitToCelsius(32);
-
+        Calculator calculator = new Calculator();
         LogicalOprations logicalOprations = new LogicalOprations();
         Scanner scan = new Scanner(System.in);
 
+        System.out.println("Find the avarage for three numbers");
         System.out.println("Enter number one ");
         int nrOne = scan.nextInt();
         System.out.println("Enter number two ");
         int nrTwo = scan.nextInt();
         System.out.println("Enter number three ");
         int nrThree = scan.nextInt();
+        System.out.println("The average for " + nrOne + ", "+ nrTwo + ", "+ nrThree + ", "+ " is: " + calculator.averageThreeNumbers(nrOne,nrTwo,nrThree));
+        System.out.println("**************************NEXT EXERCISE************************************");
+        System.out.println();
+
+        System.out.println("Display the division Modulo");
+        System.out.println("Enter number one ");
+        nrOne = scan.nextInt();
+        System.out.println("Enter number two ");
+        nrTwo = scan.nextInt();
+        System.out.println("Dividing " + nrOne + " to " + nrTwo + " the modulo is " + calculator.divisionModuloDisplay(nrOne,nrTwo));
+        System.out.println("**************************NEXT EXERCISE************************************");
+        System.out.println();
+
+        System.out.println("Convert Fahrenheit to Celsius");
+        System.out.println("Enter temperature in Fahrenheit ");
+        nrOne = scan.nextInt();
+        System.out.println("Temperature " + nrOne + " in Fahrenheit is " + calculator.fahrenheitToCelsius(nrOne) + " in Celsius ");
+        System.out.println("**************************NEXT EXERCISE************************************");
+        System.out.println();
+
+        System.out.println("Convert inch to meter");
+        System.out.println("Enter distance in inch");
+        nrOne = scan.nextInt();
+        System.out.println("Distance " + nrOne + " in inch is " + calculator.inchToMeter(nrOne) + " in meters ");
+        System.out.println("**************************NEXT EXERCISE************************************");
+        System.out.println();
+
+        System.out.println("Calculate Speed");
+        System.out.println("Enter Distance in Meteres");
+        nrOne = scan.nextInt();
+        System.out.println("Enter duration in hours ");
+        nrTwo = scan.nextInt();
+        System.out.println("Enter duration in minutes ");
+        nrThree = scan.nextInt();
+        System.out.println("Enter duration in seconds ");
+        int nrFour = scan.nextInt();
+        System.out.println("YOUR SPEED IS:");
+        calculator.speedCalculator(nrOne,nrTwo,nrThree,nrFour);
+
+        System.out.println("Enter number one ");
+        nrOne = scan.nextInt();
+        System.out.println("Enter number two ");
+        nrTwo = scan.nextInt();
+        System.out.println("Enter number three ");
+        nrThree = scan.nextInt();
         System.out.println("The biggest number is: " + logicalOprations.compareThreeNumbers(nrOne,nrTwo,nrThree));
 
         System.out.println("Enter age one ");
         int age = scan.nextInt();
         System.out.println("Are you allowed to vote: " + logicalOprations.isEligibleToVote(age));
-    }
-
-    public static float conversieFahrenheitToCelsius(float gradeFahrenheit){
-        float gradeCelcius = ((float) 5/9 ) * (gradeFahrenheit - 32);
-        System.out.println( gradeFahrenheit + " grade F este " + gradeCelcius + " grade C");
-        return gradeCelcius;
     }
 }
