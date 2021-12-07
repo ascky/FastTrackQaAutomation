2ce2af5b7816ccdebc9c8b92bb53d011da363a5e
Cristian Astilean <cristian.astilean@itiviti.com>
2021-12-07 15:41:07 +0200

    Homework Day1 Part1 Ex 1-4

---
+++ src/HomeworkDay1_1/Homework1.java
@@ -0,0 +1,27 @@
+package HomeworkDay1_1;
+
+public class Homework1 {
+
+    public static void main(String[] args) {
+
+        //Exercise 1 - Print Hello in console and on a new line print your name
+        System.out.println(" --------------- Exercise 1 ----------------");
+        System.out.println("Hello");
+        System.out.println("Astilean Cristian");
+
+        // Exercise 2 - Print the result of the sum of two numbers
+        System.out.println(" --------------- Exercise 2 ----------------");
+        System.out.println("The sum of 15 + 31 = " + (15 + 31));
+
+        // Exercise 3 - Print the result of the division of two numbers
+        System.out.println(" --------------- Exercise 3 ----------------");
+        System.out.println("10 divide to 3 is = " + (10 / 3f));
+
+        //Exercise 4 - do the following calculations
+        System.out.println(" --------------- Exercise 4 ----------------");
+        System.out.println("a).  -5 + 8 * 6 = " + (-5 + 8 * 6));
+        System.out.println("b). (55 + 9) % 9 = " + ((55 + 9) % 9));
+        System.out.println("c). 20 + -3 * 5 / 8 = " + (20 + -3 * 5d / 8));
+        System.out.println("d). 5 + 15/3 * 2 - 8 % 3 = " + (5 + 15/3d * 2 - 8 % 3));
+    }
+}
